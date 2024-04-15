a5a793d86fbc7fe66bdf394601f5da19e804fb3b
Eyob14 <eyobzebene3580@gmail.com>
2024-04-16 00:09:57 +0300

    sentary basic setup and logger

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,6 +3,7 @@ export const apiPath = import.meta.env.VITE_API_PATH as string
 export const apiBase = `${apiOrigin}${apiPath}`
 export const cloudinaryCloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME as string
 export const cloudinaryPreset = import.meta.env.VITE_CLOUDINARY_PRESET as string
+export const sentaryUrl = import.meta.env.VITE_SENTARY_URL as string
 
 if (typeof apiOrigin !== 'string') {
   throw new Error('VITE_API_ORIGIN is not defined')
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,17 +5,21 @@ import { createDatabase } from './database'
 import config from './config'
 import { User } from './entities/user'
 import { fakeAdmin } from './entities/tests/fakes'
+import logger from './logger'
 
 const database = createDatabase(config.database as DataSourceOptions)
 
 database.initialize().then(async () => {
+  logger.info('Database initialized')
   const app = createApp(database)
+  logger.info('Checking for admin user...')
   const adminUser = await database.getRepository(User).findOne({
     where: {
       role: 'admin',
     },
   })
   if (!adminUser) {
+    logger.info('Admin not found. Creating admin for the first time...')
     const adminData = fakeAdmin({
       email: config.auth.adminEmail,
       password: config.auth.adminPassword,
@@ -25,9 +29,12 @@ database.initialize().then(async () => {
       config.auth.passwordCost
     )
     await database.getRepository(User).save(adminData)
+    logger.info('Admin is created successfully')
+  } else {
+    logger.info('Admin already exists')
   }
   app.listen(config.port, () => {
     // eslint-disable-next-line no-console
-    console.log(`Server is running at http://localhost:${config.port}`)
+    logger.info(`Server is running at http://localhost:${config.port}`)
   })
 })
